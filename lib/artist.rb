dbc491268efb8bb83b4ccb0e8c9947266bb69cbd
Christopher Leja <christopher.leja@gmail.com>
2020-02-22 17:40:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,12 @@
 class Artist 
 
-    @@songs = []
 attr_accessor :name, :songs, :title, :artist_name
   
   def initialize(name)
     @name = name
     @artist_name = self.name
-    @@songs << self
+    @songs = []
+    @songs << self
   end
   
   def add_song(name)
