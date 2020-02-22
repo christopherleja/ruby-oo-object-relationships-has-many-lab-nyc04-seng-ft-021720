717c91a180e21c60347698a4cfd2e4c6a9e3fb56
Christopher Leja <christopher.leja@gmail.com>
2020-02-22 19:15:52 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,8 +18,12 @@ class Author
     end
   end
   
-  def add_post(post)
-    Post.new(post)
+  def add_post
+    post.author = self
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
     post.author = self
   end
     
